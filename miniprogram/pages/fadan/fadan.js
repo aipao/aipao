09199e7569379308f104a897a70bf832d7024e9d
wangjianuo123 <50947798+wangjianuo123@users.noreply.github.com>
2019-05-25 16:27:44 +0800

    用户id

--- miniprogram/pages/fadan/fadan.js
+++ miniprogram/pages/fadan/fadan.js
@@ -1,56 +1,42 @@
 Page({
   data: {
     showTopTips: false,
+    campus: "1",
+    name: "",
+    dormitory: "",
+    district: "",
+    dormnumber:"",
+    telephone:"",
+    reserve_telephone: "",
+    content: "",
 
     radioItems: [
       { name: 'cell standard', value: '0' },
       { name: 'cell standard', value: '1', checked: true }
     ],
     checkboxItems: [
-      { name: '东校区', value: '0', checked: true },
+      { name: '东校区', value: '0'},
       { name: '西校区', value: '1' }
     ],
-
     date: "2016-09-01",
     time: "12:01",
-
-    countryCodes: ["+86", "+80", "+84", "+87"],
-    countryCodeIndex: 0,
-
-    countries: ["中国", "美国", "英国"],
-    countryIndex: 0,
-
-    accounts: ["微信号", "QQ", "Email"],
-    accountIndex: 0,
-
     isAgree: false
   },
-  showTopTips: function () {
-    var that = this;
+  radio: function (e) {
     this.setData({
-      showTopTips: true
-    });
-    setTimeout(function () {
-      that.setData({
-        showTopTips: false
-      });
-    }, 3000);
+      guige_key02: e.currentTarget.dataset.id
+    })
+    console.log(e.currentTarget.dataset.id)
   },
+  // 发货地址选择,获取用户选择的单选框的值
   radioChange: function (e) {
-    console.log('radio发生change事件，携带value值为：', e.detail.value);
-
-    var radioItems = this.data.radioItems;
-    for (var i = 0, len = radioItems.length; i < len; ++i) {
-      radioItems[i].checked = radioItems[i].value == e.detail.value;
-    }
-
     this.setData({
-      radioItems: radioItems
-    });
+      arr_guige02: e.detail.value
+    })
+    console.log(e.detail.value)
   },
   checkboxChange: function (e) {
     console.log('checkbox发生change事件，携带value值为：', e.detail.value);
-
     var checkboxItems = this.data.checkboxItems, values = e.detail.value;
     for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
       checkboxItems[i].checked = false;
@@ -64,43 +50,21 @@ Page({
     }
 
     this.setData({
-      checkboxItems: checkboxItems
+      checkboxItems: checkboxItems,
+      campus:e.detail.value
+    });
+    console.log(this.data.campus)
+  },
+  tijiao: function () {
+    wx.showModal({
+      content: '弹窗内容，告知当前状态、信息和解决方法，描述文字尽量控制在三行内',
+      showCancel: false,
+      success: function (res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+        }
+      }
     });
   },
-  bindDateChange: function (e) {
-    this.setData({
-      date: e.detail.value
-    })
-  },
-  bindTimeChange: function (e) {
-    this.setData({
-      time: e.detail.value
-    })
-  },
-  bindCountryCodeChange: function (e) {
-    console.log('picker country code 发生选择改变，携带值为', e.detail.value);
-
-    this.setData({
-      countryCodeIndex: e.detail.value
-    })
-  },
-  bindCountryChange: function (e) {
-    console.log('picker country 发生选择改变，携带值为', e.detail.value);
-
-    this.setData({
-      countryIndex: e.detail.value
-    })
-  },
-  bindAccountChange: function (e) {
-    console.log('picker account 发生选择改变，携带值为', e.detail.value);
-
-    this.setData({
-      accountIndex: e.detail.value
-    })
-  },
-  bindAgreeChange: function (e) {
-    this.setData({
-      isAgree: !!e.detail.value.length
-    });
-  }
+  
 });
